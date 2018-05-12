5e12b7f6438c5a242942fe415a9f4c1317e3a173
zicheng-pan <1094275633@qq.com>
2018-05-12 14:02:03 +0800

    second submit change style and commit submit form

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 from tool import sendemail
+import json
+from flask import render_template,Flask,redirect,url_for,request
+
+app = Flask('welcome-portal')
+app.secret_key = 'shiyanlou'
+
+email_list = ["programvip@sina.com",]
+
+@app.route('/',methods=["GET","POST"])
+def home():
+    return render_template("index.html")
+
+@app.route('/submit',methods=["GET","POST"])
+def post():
+    user_name = request.form.get("user_name")
+    user_email = request.form.get("user_email")
+    user_title = request.form.get("user_title")
+    user_content = request.form.get("user_content")
+    data = {}
+    data["name"] = user_name
+    data["title"] = user_title
+    data["email"] = user_email
+    data["content"] = user_content
+    json_str = json.dumps(data)
+    sendemail._to = sendemail._to + ','.join(email_list)
+    sendemail.send(json_str)
+    return render_template("index.html",addition_script="submit")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port=8080)
+
--- tool/sendemail.py
+++ tool/sendemail.py
@@ -2,15 +2,15 @@ import smtplib
 from email.mime.text import MIMEText
 _user = "wangxiao084@163.com"
 _pwd  = "w123456"
-_to   = "programvip@sina.com"
+_to   = ""
 
-def send():
-    #json str
-    msg = MIMEText("Test")
-    msg["Subject"] = "don't panic"
+# json str
+
+def send(message):
+    msg = MIMEText(message or "Error Message")
+    msg["Subject"] = "New message from portal"
     msg["From"] = _user
     msg["To"] = _to
-
     try:
         s = smtplib.SMTP_SSL("smtp.163.com", 465)
         s.login(_user, _pwd)
